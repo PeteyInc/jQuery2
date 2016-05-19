a06cc9a2c4ed2becf99368455a2c5cbf8231cacf
Jared M. Peterson <jared@peteyinc.com>
2016-05-19 15:07:28 -0600

    fixed

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -1,9 +1,46 @@
-$(document).ready(function(){
-  var listo = [];
+$(document).ready(function() {
+    var listo = [];
+    $('#newTaskForm').hide();
 
-  var Task = function(task) {
-    this.task = task;
-    this.id = 'new';
-  };
+    var Task = function(task) {
+        this.task = task;
+        this.id = 'new';
+    };
 
-});
+    var addTask = function(task) {
+        if (task) {
+            task = new Task(task);
+            listo.push(task);
+            $('#newItemInput').val('');
+            $('#newList').append(
+                '<a href="#finish" class="" id="item">' +
+                '<li class="list-group-item">' +
+                '<h3>' + task.task + '</h3>' +
+                '<span class="arrow pull-right">' +
+                '<i class="glyphicon glyphicon-arrow-right">' +
+                '</span>' +
+                '</li>' +
+                '</a>'
+            );
+        };
+        $('#newTaskForm').slideToggle('fast', 'linear');
+    }; // addTask
+
+    $('#saveNewItem').on('click', function(e) {
+        e.preventDefault();
+        var task = $('#newItemInput').val().trim();
+        addTask(task);
+    });
+
+    $('#add-todo').on('click', function() {
+        $('#newTaskForm').fadeToggle('fast', 'linear');
+    });
+
+    $('#cancel').on('click', function(e) {
+        e.preventDefault();
+        $('#newTaskForm').fadeToggle('fast', 'linear');
+    });
+
+
+
+}); // doc ready
